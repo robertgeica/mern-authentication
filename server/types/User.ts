eed897af1e6fab18a491ad96437334f41be0507e
robertgeica <geicarobert@gmail.com>
2023-02-19 18:53:29 +0100

    add user ts interface

---
+++ server/types/User.ts
@@ -0,0 +1,20 @@
+import { Document, Types } from 'mongoose';
+
+export interface IUser extends Document {
+  _id: Types.ObjectId;
+  schema_version: string;
+  name: string;
+  email: string;
+  password: string;
+  role: 'admin' | 'user';
+  phoneNumber?: string;
+  avatarUrl?: string;
+  twoFactorAuthEnabled: boolean;
+  twoFactorAuthCode?: number;
+  
+  isEmailConfirmed: boolean;
+  emailConfirmationToken?: string;
+  emailConfirmationExpire?: Date;
+  resetPasswordToken?: string;
+  resetPasswordExpire?: Date;
+}
