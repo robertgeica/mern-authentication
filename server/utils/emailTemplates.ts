4900b4445a73f1715a44b6f267d3d80954a70ba9
robertgeica <geicarobert@gmail.com>
2023-02-20 21:10:00 +0100

    add reset password email template

--- server/utils/emailTemplates.ts
+++ server/utils/emailTemplates.ts
@@ -1,9 +1,19 @@
-const confirmAccountEmail = (resetUrl: string): string => {
+const confirmAccountEmail = (confirmUrl: string): string => {
   return `
   <h1>Welcome aboard</h1>
   <p>Access the following link to confirm your account:</p>
+  <a href=${confirmUrl} clicktracking=off>${confirmUrl}</a>
+`;
+};
+
+const resetPasswordEmail = (resetUrl: string): string => {
+  return `
+  <h1>Password reset</h1>
+  <p>Access the following link to reset your password:</p>
   <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
 `;
 };
 
-export { confirmAccountEmail };
+
+
+export { confirmAccountEmail, resetPasswordEmail };
