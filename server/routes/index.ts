cf2c994f1b7904d3ba987475f9ba68696c66cdb6
robertgeica <geicarobert@gmail.com>
2023-02-27 23:26:45 +0100

    add dotenv example

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -26,20 +26,26 @@ import {
 
 const router: Router = Router();
 
-router.route('/api/v1/users').post(createUser);
-router.route('/api/v1/users/login').post(loginUser);
-
-router.route('/api/v1/users/logged-user').get(protect, getLoggedUser);
-router.route('/api/v1/users/:id').get(protect, getUser);
 router
   .route('/api/v1/users')
-  .delete(protect, deleteUser)
-  .patch(protect, updateUser);
+  .post(createUser)
+  .patch(protect, updateUser)
+  .delete(protect, deleteUser);
 
-  router.route('/api/v1/users/confirm-email').post(requestEmailConfirmation);
+router.route('/api/v1/users/login').post(loginUser);
+
+router.route('/api/v1/users/:id').get(protect, getUser);
+router.route('/api/v1/users/logged-user').get(protect, getLoggedUser);
+
+router.route('/api/v1/users/confirm-email').post(requestEmailConfirmation);
 router.route('/api/v1/users/confirm-email/:token').put(confirmEmail);
+
 router.route('/api/v1/users/reset-password').post(requestPasswordReset);
 router.route('/api/v1/users/reset-password/:token').put(confirmPasswordReset);
+
+router.route('/api/v1/users/change-email').post(protect, requestEmailChange);
+router.route('/api/v1/users/change-email/:token').post(confirmEmailChange);
+
 router
   .route('/api/v1/users/image-upload')
   .patch(
@@ -51,11 +57,5 @@ router
     allowMultiple(false),
     uploadUserImage
   );
-
-  router.route('/api/v1/users/change-email').post(protect, requestEmailChange);
-  router.route('/api/v1/users/change-email/:token').post(confirmEmailChange);
-
-
-
 
 export default router;
