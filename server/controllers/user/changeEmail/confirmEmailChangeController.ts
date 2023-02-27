292ef2909193868bc4cf6809464579b2dbcea7c8
robertgeica <geicarobert@gmail.com>
2023-02-27 22:10:23 +0100

    add confirm email change controller

---
+++ server/controllers/user/changeEmail/confirmEmailChangeController.ts
@@ -0,0 +1,100 @@
+import { Response, Request } from 'express';
+import crypto from 'crypto';
+import User from '../../../models/User';
+import ErrorResponse from '../../../utils/errorResponse';
+import asyncHandler from '../../../middleware/asyncHandler';
+import sendEmail from '../../../modules/emailSender';
+import { confirmChangeEmail, confirmEmailChanged } from '../../../utils/emailTemplates';
+import { changeEmailUrl } from '../../../utils/changeEmailUrl';
+
+// @route         PUT /api/v1/users/change-email/:token
+// @description   Confirm email change
+// @access        Public
+export const confirmEmailChange = asyncHandler(
+  async (req: Request, res: Response, next: Function): Promise<void> => {
+    const changeEmailToken = crypto
+      .createHash('sha256')
+      .update(req.params.token)
+      .digest('hex');
+
+    let user: any = await User.findOne({
+      $or: [
+        {
+          changeEmailStepOneToken: changeEmailToken,
+          changeEmailStepOneExpire: { $gt: Date.now() },
+        },
+        {
+          changeEmailStepTwoToken: changeEmailToken,
+          changeEmailStepTwoExpire: { $gt: Date.now() },
+        },
+      ],
+    }).select('-password');
+
+    if (!user) {
+      return next(new ErrorResponse('Invalid or expired token.', 400));
+    }
+
+    if (user.changeEmailStepOneToken === changeEmailToken) {
+      user.isEmailChangeStepOneConfirmed = true;
+      user.changeEmailStepOneToken = undefined;
+      user.changeEmailStepOneExpire = undefined;
+
+      const emailChangeConfirmationToken = user.generateEmailChangeToken(
+        'changeEmailStepTwoToken',
+        'changeEmailStepTwoExpire'
+      );
+
+      try {
+        const confirmChangeEmailUrl = changeEmailUrl(
+          emailChangeConfirmationToken
+        );
+
+        await sendEmail({
+          to: user.newEmail,
+          subject: 'Confirm your email change 2/2',
+          html: confirmChangeEmail(confirmChangeEmailUrl),
+        });
+
+        await user.save();
+
+        res.status(200).json({
+          success: true,
+          message: 'Change email confirmation request was sent successfully.',
+        });
+      } catch (error) {
+        user.changeEmailStepTwoToken = undefined;
+        user.changeEmailStepTwoExpire = undefined;
+        await user.save();
+
+        return next(new ErrorResponse('Email could not be sent', 500));
+      }
+    }
+    
+    if (user.changeEmailStepTwoToken === changeEmailToken) {
+      const oldEmail = user.email;
+
+      user.email = user.newEmail;
+      user.newEmail = undefined;
+      user.isEmailChangeStepOneConfirmed = undefined;
+      user.changeEmailStepTwoToken = undefined;
+      user.changeEmailStepTwoExpire = undefined;
+
+      try {
+        await sendEmail({
+          to: oldEmail,
+          subject: 'Email change confirmed',
+          html: confirmEmailChanged(),
+        });
+
+        await user.save();
+
+        res.status(201).json({
+          success: true,
+          message: 'Your email was changed successfully.',
+        });
+      } catch (error) {
+        return next(new ErrorResponse('Email could not be sent', 500));
+      }
+    }
+  }
+);
