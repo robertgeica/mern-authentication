3ebd43d2e983df25dbde1bdc69035454ae5b8ad5
robertgeica <geicarobert@gmail.com>
2023-03-25 01:27:17 +0100

    add home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div className='wave-wrapper'>
+      <div className='page-container'>
+        <h1 className='home-title'>MERN Stack authentication system</h1>
+        <p className='home-description'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel
+          vehicula ante. Nam vulputate velit nec purus consectetur malesuada.
+          Nullam convallis eleifend ultrices.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
